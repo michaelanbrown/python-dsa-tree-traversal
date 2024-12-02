5cf909da593d1ee879e62a5d84ef9925327997eb
michaelanbrown <m_brownbrown@yahoo.com>
2024-12-02 13:12:43 -0500

    pseudocode

--- lib/tree.py
+++ lib/tree.py
@@ -1,3 +1,11 @@
+#Initialize an empty output list
+#Initialize an list of nodes to visit and add the root node to it
+#While there are nodes in the nodes to visit list
+  #Remove the first node from the nodes to visit list
+  #Add its value to the output list
+  #Add its children (if any) to the end of the nodes to visit list
+#Return the output list
+
 class Tree:
   def __init__(self, root = None):
     self.root = root
