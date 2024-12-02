8a07af2329052f09311608c769acf9cdf042a687
michaelanbrown <m_brownbrown@yahoo.com>
2024-12-02 13:18:59 -0500

    Added depth first code solution

--- lib/tree.py
+++ lib/tree.py
@@ -65,3 +65,17 @@ print(breadth_first_traversal(root))
   #Add its value to the output list
   #Add its children (if any) to the BEGINNING of the list of nodes to visit
 #Return the output list
+
+def depth_first_traversal(node):
+  result = []
+  nodes_to_visit = [node]
+
+  while len(nodes_to_visit) > 0:
+    # 1. Remove the first node from the `nodes_to_visit` list
+    node = nodes_to_visit.pop(0)
+    # 2. Add its value to the result list
+    result.append(node['value'])
+    # 3. Add its children (if any) to the BEGINNING of the `nodes_to_visit` list
+    nodes_to_visit = node['children'] + nodes_to_visit
+
+  return result
